85085c2cac08f5c1e6a30cae2a3e2316139e2f0c
Artem Mokhov <teumik@gmail.com>
2022-12-14 23:55:44 +0400

    init: create template project

---
+++ src/components/app/App.tsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import Footer from '../footer/footer';
+import Header from '../header/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>Header More Headers And I Like It</h1>
+      <p>Text</p>
+      <section>
+        <h2>Subheader</h2>
+        <p>Subtext</p>
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
