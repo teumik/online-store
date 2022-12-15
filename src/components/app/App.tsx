090a5942534b00431ea5b53722c0cf0fb18bf210
Artem Mokhov <teumik@gmail.com>
2022-12-15 18:03:21 +0400

    refactor: rename components

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss';
-import Footer from '../footer/footer';
-import Header from '../header/header';
+import Footer from '../Footer/footer';
+import Header from '../Header/header';
 
 function App() {
   return (
