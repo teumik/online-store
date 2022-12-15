b44012b04e0f65de97fb7ef371fb211e3e5dcea8
Artem Mokhov <teumik@gmail.com>
2022-12-16 00:42:49 +0400

    feat: implement footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,34 @@
 import './footer.scss';
+import RssSvg from './RssSvg/rssSvg';
+import GithubSvg from './GithubSvg/githubSvg';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="small">Малой</div>
-      <div>2022</div>
-      <div className="drew">Андрей</div>
+      <ul className="footer__list">
+        <li className="footer__items">
+          <ul className="footer__list git">
+            <li className="footer__items git__tems">
+              <a className="footer__links" href="https://github.com/teumik" target="_blank" rel="noreferrer">
+                <GithubSvg />
+              </a>
+            </li>
+            <li className="footer__items git__tems">
+              <a className="footer__links" href="https://github.com/AndreyVeres" target="_blank" rel="noreferrer">
+                <GithubSvg />
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className="footer__items">{`${new Date().getFullYear()} ©DreamTeam`}</li>
+        <li className="footer__items">
+          <a className="footer__links" href="https://rs.school/js/" target="_blank" rel="noreferrer">
+            <div className="wrapper_adjust">
+              <RssSvg />
+            </div>
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
