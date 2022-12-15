dca79517e82304028845433cfb61ebfe2365836b
Artem Mokhov <teumik@gmail.com>
2022-12-16 00:42:26 +0400

    refactor: add attributes to link and styles

--- src/components/header/Logo/logo.tsx
+++ src/components/header/Logo/logo.tsx
@@ -3,7 +3,7 @@ import './logo.scss';
 function Logo() {
   return (
     <div className="logo">
-      <a className="logo__link" href="./">
+      <a className="logo__link" href="https://www.google.com/" target="_blank" rel="noreferrer">
         <div className="logo__svg"><span>LOGO</span></div>
         <h1 className="logo__header">Online Shop</h1>
       </a>
