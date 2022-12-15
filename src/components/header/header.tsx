ece0601eee6b028f53398d2d341b4b63eec58f23
Artem Mokhov <teumik@gmail.com>
2022-12-15 19:09:44 +0400

    feat: add Cart  to Header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,24 @@
 import './header.scss';
+import Cart from './Cart/cart';
 
 function Header() {
+  const totalPrice = 1000;
+  const currency = '$';
+  const itemsCount = 3;
+  const totalDiscount = 2;
   return (
     <header className="header">
-      <div className="header__logo">Logo</div>
-      <div className="header__cart">Cart</div>
+      <ul className="header__list">
+        <li className="header__items" />
+        <li className="header__items">
+          <Cart
+            currency={currency}
+            totalPrice={totalPrice}
+            totalDiscount={totalDiscount}
+            itemsCount={itemsCount}
+          />
+        </li>
+      </ul>
     </header>
   );
 }
