43d1a579ef2f6ac32237a5ba156d195a850be92c
Andrey <spacecmetanka@gmail.com>
2022-12-16 16:29:53 +0300

    refactor: adding summary in cartPage

--- src/components/cart/cartPage.tsx
+++ src/components/cart/cartPage.tsx
@@ -1,9 +1,12 @@
 import CartList from './cartList/cartList';
+import CartSummary from './cartSummary/cartSummary';
+import './cartPage.scss'
 
 function CartPage() {
   return (
     <div className="cart">
       <CartList />
+      <CartSummary />
     </div>
   );
 }
