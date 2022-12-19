062de833f78d9bba6a64c4e52913a85989d55530
Andrey <spacecmetanka@gmail.com>
2022-12-20 02:04:07 +0300

    refactor: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Filters from './components/MainPage/Filters/Filters';
-import productsData from './lib/data/productsData.json';
+import Filters from './container/MainPage/Filters/Filters';
+
 import Products from './components/MainPage/Products/Products';
-import DataController from './controller/data.controller';
+
+import DC from './controller/data.controller';
 
 function App() {
-  const dataController = new DataController(productsData.products);
-  const data = dataController.getData;
+  const data = DC.getData;
 
   return (
     <>
       <Header />
       <main className="main">
-        <Filters
-          categories={dataController.getUniqCategories()}
-          brands={dataController.getUniqBrands()}
-        />
+        <Filters />
         <Products data={data} />
       </main>
       <Footer />
