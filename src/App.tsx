2168f98636fb05a370299cbb832da8c317517580
Artem Mokhov <teumik@gmail.com>
2022-12-19 21:05:48 +0400

    refactor: file structure and paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.scss';
-import Footer from './components/view/Footer/Footer';
-import Header from './components/view/Header/Header';
-import Filters from './components/view/MainPage/Filters/Filters';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Filters from './components/MainPage/Filters/Filters';
 import productsData from './lib/data/productsData.json';
-import Products from './components/view/MainPage/Products/Products';
-import DataController from './components/controller/data.controller';
+import Products from './components/MainPage/Products/Products';
+import DataController from './controller/data.controller';
 
 function App() {
   const dataController = new DataController(productsData.products);
