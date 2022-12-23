b1d09dc374a7243957480d335f11a3233d2db047
Andrey <spacecmetanka@gmail.com>
2022-12-24 00:43:04 +0300

    feat: create useUpdateProducts

--- src/hooks/useUpdateProducts.ts
+++ src/hooks/useUpdateProducts.ts
@@ -1,12 +1,11 @@
-import { ReactNode, useContext, useState } from 'react';
+import { useContext, useState } from 'react';
 import DataContext from '../context/data.context';
 
 function useUpdateProducts() {
   const ctx = useContext(DataContext);
-  const [productsState, updateState] = useState(ctx.view);
+  const [productsState, updateState] = useState(ctx);
   const updateProducts = () => {
-    const ctxCopy = Object.create(ctx.view);
-    console.log(ctx.view);
+    const ctxCopy = Object.create(ctx);
     updateState(ctxCopy);
   };
 
