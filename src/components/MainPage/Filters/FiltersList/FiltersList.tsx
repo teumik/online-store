54d089b0e3ca35f1a42cdb29d9117a6deec06eb0
Andrey <spacecmetanka@gmail.com>
2022-12-24 00:44:29 +0300

    refactor: filterList

--- src/components/MainPage/Filters/FiltersList/FiltersList.tsx
+++ src/components/MainPage/Filters/FiltersList/FiltersList.tsx
@@ -1,29 +1,31 @@
-import './filtersList.scss';
 import { useContext } from 'react';
-import { filterListProps } from '../../../../types/types';
 import DataContext from '../../../../context/data.context';
-import useUpdateProducts from '../../../../hooks/useUpdateProducts';
-// import useUpdateProducts from '../../../../hooks/useUpdateProducts';
+import './filtersList.scss';
 
-function FiltersListView({ filterTitle, values }: filterListProps) {
+export interface filterListPropsView {
+  filterTitle: string;
+  values: string[];
+  inputHandler: (title: string, categoryTitle: string) => void;
+}
+
+export default function FiltersListView({
+  filterTitle,
+  values,
+  inputHandler,
+}: filterListPropsView) {
   const ctx = useContext(DataContext);
-  const { updateProducts } = useUpdateProducts();
-  const changeHandler = (title: string) => {
-    ctx.onChangeFiltersList(title);
-    // console.log(ctx.view);
-    updateProducts();
-  };
   return (
     <div className="filters__item">
       <h3 className="filters__title">{filterTitle}</h3>
       <div className="filters__list">
-        {values.map((title) => (
+        {values.sort().map((title) => (
           <li className="filters__list-item" key={title}>
-            <input onChange={() => changeHandler(title)} type="checkbox" className="filters__list-checkbox" id={title} />
+            <input onChange={() => inputHandler(title, filterTitle)} type="checkbox" className="filters__list-checkbox" id={title} />
             <label htmlFor={title} className="filters__list-label">{` ${title} `}</label>
             <p className="filters__list-stock">
-              5/$
-              {/* {ctx.maxLength(title)} */}
+              {ctx.getCurrentSense(title)}
+              /
+              {ctx.getMaxSense(title)}
             </p>
           </li>
         ))}
@@ -31,5 +33,3 @@ function FiltersListView({ filterTitle, values }: filterListProps) {
     </div>
   );
 }
-
-export default FiltersListView;
