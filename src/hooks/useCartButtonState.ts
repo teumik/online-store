374040139d9654f40d42650d59b8edb8735121c6
Andrey <spacecmetanka@gmail.com>
2022-12-30 18:37:59 +0300

    refactor: update useCartButtonState

--- src/hooks/useCartButtonState.ts
+++ src/hooks/useCartButtonState.ts
@@ -1,30 +1,33 @@
-import { useContext, useState } from 'react';
-import DataContext from '../context/data.context';
+import { useState } from 'react';
+import { dataController } from '../controller/data.controller';
 
 function useCartButtonState() {
   const [{ isActive, isHover }, toggleState] = useState({ isActive: false, isHover: false });
-  const ctx = useContext(DataContext);
+  const ctx = dataController;
 
   const buttonHandler = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    event?: React.MouseEvent<HTMLButtonElement, MouseEvent>,
     isInCart?: boolean
   ) => {
     if (event) {
       if (event.type === 'mouseenter' || event.type === 'mouseleave') {
         toggleState((state) => ({ ...state, isHover: !state.isHover }));
+        return;
       }
       if (event.type === 'click') {
-        if (isInCart && !isActive) {
-          ctx.setCartItem(event.currentTarget.id);
-          return;
-        }
-        ctx.setCartItem(event.currentTarget.id);
+        const { id } = event.currentTarget;
+        ctx.setCartItem(id);
+        if (isInCart && !isActive) return;
         toggleState((state) => ({ ...state, isActive: !state.isActive }));
       }
     }
   };
 
   const getInnerText = (isInCart: boolean) => {
+    if (isActive && !isInCart) {
+      return 'Add';
+    }
+
     if (isActive || isInCart) {
       if (isHover && isActive) return 'Remove';
       if (isHover && isInCart) return 'Remove';
@@ -33,11 +36,23 @@ function useCartButtonState() {
     return 'Add';
   };
 
+  const getCount = (id: number) => ctx.getItemCount(id);
+
+  const increaseItemCount = (idValue: number) => {
+    ctx.increaseItemCount(idValue);
+  };
+  const decreaseItemCount = (idValue: number) => {
+    ctx.decreaseItemCount(idValue);
+  };
+
   return {
     isActive,
     isHover,
     buttonHandler,
     getInnerText,
+    increaseItemCount,
+    decreaseItemCount,
+    getCount,
   };
 }
 
