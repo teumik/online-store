e57e6f1526449710593993c4572aeab97cdfad08
Andrey <spacecmetanka@gmail.com>
2023-01-06 16:58:54 +0300

    fix: show message empty cart

--- src/container/CartPage/CartPage.tsx
+++ src/container/CartPage/CartPage.tsx
@@ -1,10 +1,8 @@
 import { useContext } from 'react';
 import CartPageView from '../../components/CartPage/CartPageView';
-import DataContext from '../../context/data.context';
+import CartContext from '../../context/cart.context';
 
 export default function CartPage() {
-  const ctx = useContext(DataContext);
-  const hasItems = ctx.cart.idArray.length > 0;
-
-  return <CartPageView hasItems={hasItems} />;
+  const { cartState } = useContext(CartContext);
+  return <CartPageView hasItems={cartState.cartItemsCount > 0} />;
 }
