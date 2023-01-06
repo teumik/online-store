1839f8dcb65f9b66d8c0260c098c4bc79b9bb1d5
Artem Mokhov <teumik@gmail.com>
2023-01-06 13:03:18 +0400

    feat: implement Code Error component

---
+++ src/components/CodeErrorPage/CodeErrorPage.tsx
@@ -0,0 +1,20 @@
+import './codeErrorPage.scss';
+
+function CodeErrorPage() {
+  return (
+    <section className="code-error">
+      <p
+        className="code-error__message"
+      >
+        It Was At This Moment That He Knew... He Fucked Up
+      </p>
+      <p
+        className="code-error__message code-error__message_god"
+      >
+        Please, reload Page and pray to the Lord
+      </p>
+    </section>
+  );
+}
+
+export default CodeErrorPage;
