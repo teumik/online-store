3131b72df66c5089e7d91cea2a4d9c6e022bebf0
Andrey <spacecmetanka@gmail.com>
2023-01-09 00:00:58 +0300

    refactor: using container for filtersRange

--- src/components/MainPage/Filters/FiltersRange/FilterRangeView.tsx
+++ src/components/MainPage/Filters/FiltersRange/FilterRangeView.tsx
@@ -1,27 +1,28 @@
-import useRangeValues from '../../../../hooks/useRangeValues';
 import './filterRange.scss';
 
-export interface filterRangeProps {
+export interface filterRangeViewProps {
   title: string;
   min: number;
   max: number;
+  calculateInputValue: (e: { target: HTMLInputElement }) => void;
+  lowprice: number;
+  maxprice: number;
 }
 
 export default function FilterRangeView({
-  title, min, max,
-}: filterRangeProps) {
-  const {
-    lowprice,
-    maxprice,
-    calculateInputValue,
-  } = useRangeValues(min, max, title);
-
+  title,
+  min,
+  max,
+  calculateInputValue,
+  lowprice,
+  maxprice,
+}: filterRangeViewProps) {
   return (
     <div className="filters__item">
       <h3 className="filters__title">{title}</h3>
       <div className="filters__range-values">
         <p>{lowprice}</p>
-        ?
+        -
         <p>{maxprice}</p>
       </div>
       <div className="range__box">
