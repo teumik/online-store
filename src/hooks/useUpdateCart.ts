92220de0a25444aabd77175e3b0f2c6c0c3306ea
Artem Mokhov <teumik@gmail.com>
2023-01-08 04:00:42 +0400

    feat: add cart storage

--- src/hooks/useUpdateCart.ts
+++ src/hooks/useUpdateCart.ts
@@ -3,8 +3,12 @@ import DataContext from '../context/data.context';
 
 function useUpdateCart() {
   const ctx = useContext(DataContext);
+  const storageCart = JSON.parse(localStorage.getItem('cart') || '[]');
+  Object.assign(ctx.cart.idArray, storageCart);
+
   const [cartState, updateState] = useState(ctx);
   const updateCart = () => {
+    localStorage.setItem('cart', JSON.stringify(cartState.cart.idArray));
     const ctxCopy = Object.create(ctx);
     updateState(ctxCopy);
   };
