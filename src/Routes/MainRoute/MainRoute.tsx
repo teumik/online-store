1b79c2e9ce2ad6964331baa28806bd1183cf19b4
Artem Mokhov <teumik@gmail.com>
2023-01-11 13:20:21 +0400

    fix: query in search field

--- src/Routes/MainRoute/MainRoute.tsx
+++ src/Routes/MainRoute/MainRoute.tsx
@@ -2,15 +2,20 @@ import MainPage from '../../components/MainPage/MainPage';
 import Products from '../../container/MainPage/Products/Products';
 import Filters from '../../container/MainPage/Filters/Filters';
 import Search from '../../container/Search/Search';
+import useQueryContext from '../../hooks/useQueryContext';
+import QueryContext from '../../context/query.context';
 
 function MainRoute() {
+  const query = useQueryContext();
   return (
     <MainPage className="main-route">
       <div
         className="main-route__filters-wrapper"
       >
-        <Search />
-        <Filters />
+        <QueryContext.Provider value={query}>
+          <Search />
+          <Filters />
+        </QueryContext.Provider>
       </div>
       <Products />
     </MainPage>
